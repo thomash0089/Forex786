e8c7e382d6d197733601de4e2ad8a3ff3e5b70c0
Imtiazgg <usaimtiaz@gmail.com>
2025-05-06 12:55:04 +0500

    Update Forex.py

--- Forex.py
+++ Forex.py
@@ -7,6 +7,9 @@ from datetime import datetime, timedelta
 from scipy.signal import argrelextrema
 from pytz import timezone
 
+# Set Streamlit page config first
+st.set_page_config(page_title="Forex AI Signals", layout="wide")
+
 # Add caching decorator to the function
 @st.cache
 def fetch_data(symbol, interval="15min", outputsize=200):
